1046e7e04a748f634f811fe37558497c27e6e5cf
Your Name <you@example.com>
2022-07-06 20:26:01 +0300

    Updates

---
+++ precache-manifest.1674c0a20aa18f96703340a2cb717b3a.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = [
+  {
+    "revision": "4673d014ec651531010c",
+    "url": "/MyFirstSite/static/css/main.4faa4b2f.chunk.css"
+  },
+  {
+    "revision": "4673d014ec651531010c",
+    "url": "/MyFirstSite/static/js/main.4673d014.chunk.js"
+  },
+  {
+    "revision": "f1e58ff421f995901670",
+    "url": "/MyFirstSite/static/css/1.8d89d154.chunk.css"
+  },
+  {
+    "revision": "f1e58ff421f995901670",
+    "url": "/MyFirstSite/static/js/1.f1e58ff4.chunk.js"
+  },
+  {
+    "revision": "a32fe9778cd247f970b5",
+    "url": "/MyFirstSite/static/js/runtime~main.a32fe977.js"
+  },
+  {
+    "revision": "12e4ce6d1f7133bcfdae45efe63717a3",
+    "url": "/MyFirstSite/static/media/Art_design.12e4ce6d.png"
+  },
+  {
+    "revision": "3c9267b11287a7b2ad0a9e16b9710ef0",
+    "url": "/MyFirstSite/static/media/deadly_mistakes.3c9267b1.png"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/MyFirstSite/static/media/OurPortpholio.d41d8cd9.pdf"
+  },
+  {
+    "revision": "8d21400bf844820f67af4eecfa5f3ce2",
+    "url": "/MyFirstSite/static/media/lviv_city_guide.8d21400b.png"
+  },
+  {
+    "revision": "b5c835c95a509630e3fb92725b29993c",
+    "url": "/MyFirstSite/static/media/museum_guide.b5c835c9.png"
+  },
+  {
+    "revision": "269665ccb2896f30f98ed7b024c68ae8",
+    "url": "/MyFirstSite/static/media/LiveWallpapers.269665cc.png"
+  },
+  {
+    "revision": "6849e423cfa5d6da00ca2f3b1c1da96e",
+    "url": "/MyFirstSite/static/media/GreenHousecontrolsystem.6849e423.png"
+  },
+  {
+    "revision": "761e45c5cfea0560f49c5af09c056f93",
+    "url": "/MyFirstSite/static/media/game_bacterial_war.761e45c5.png"
+  },
+  {
+    "revision": "5e308a4067e6558e183f9a417267b498",
+    "url": "/MyFirstSite/static/media/AndroidPOS.5e308a40.png"
+  },
+  {
+    "revision": "a16a9656fdaf302f6e649215ae87c43f",
+    "url": "/MyFirstSite/static/media/MagazineExperyment.a16a9656.png"
+  },
+  {
+    "revision": "b9c14050460194294e82d5da29d6d3ec",
+    "url": "/MyFirstSite/static/media/Busbookingapp.b9c14050.png"
+  },
+  {
+    "revision": "35a46094e030ea705b686401e186e547",
+    "url": "/MyFirstSite/static/media/Kelner.35a46094.png"
+  },
+  {
+    "revision": "04ff7fea7ce295c0d2905cc53e88c7de",
+    "url": "/MyFirstSite/static/media/MindMap.04ff7fea.png"
+  },
+  {
+    "revision": "f2431291ad71b3e73389fe2d06cb8e3d",
+    "url": "/MyFirstSite/static/media/earth-about.f2431291.png"
+  },
+  {
+    "revision": "544af2853ccaef7881e362cf95e60170",
+    "url": "/MyFirstSite/static/media/JuniorSalesManager.544af285.png"
+  },
+  {
+    "revision": "f5274b366f74f2e01e685fc15835f254",
+    "url": "/MyFirstSite/static/media/JuniorQAEngineer.f5274b36.png"
+  },
+  {
+    "revision": "f7677c2d9f45a4bfe0d03713a66845f9",
+    "url": "/MyFirstSite/static/media/JuniorUIUXDesigner.f7677c2d.png"
+  },
+  {
+    "revision": "3f560cd7b74a863c19ae28f34a588447",
+    "url": "/MyFirstSite/static/media/rubber-duck.3f560cd7.png"
+  },
+  {
+    "revision": "c8ebce0b4ea855992efb13e5a2f92e82",
+    "url": "/MyFirstSite/static/media/itemimg.c8ebce0b.png"
+  },
+  {
+    "revision": "f023f56b320c13ec82dcad7cff21c855",
+    "url": "/MyFirstSite/static/media/issue.f023f56b.png"
+  },
+  {
+    "revision": "9e5745f66ea1e83ded6d8952f3797642",
+    "url": "/MyFirstSite/static/media/solution.9e5745f6.png"
+  },
+  {
+    "revision": "ac745a5b2fe34ca2dc8691e938b08c40",
+    "url": "/MyFirstSite/static/media/imgbig.ac745a5b.png"
+  },
+  {
+    "revision": "c869ccc7bdc9ecc2068d5d032738b6b0",
+    "url": "/MyFirstSite/static/media/header.c869ccc7.png"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/MyFirstSite/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/MyFirstSite/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/MyFirstSite/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "7976ca76c8d527a7d358a77ab0ecf949",
+    "url": "/MyFirstSite/index.html"
+  }
+];
